d11a0010865aa6a81005f98fce7f70d94f100c18
Abdur raheem <91405474+Nyx-abu@users.noreply.github.com>
2025-04-29 23:14:08 +0530

    Delete backend/hashPassword.js

--- backend/hashPassword.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs'
-
-const hashPassword = async () => {
-  const hashedPassword = await bcrypt.hash('admin123', 10)
-  console.log('Hashed Password:', hashedPassword)
-}
-
-hashPassword()
